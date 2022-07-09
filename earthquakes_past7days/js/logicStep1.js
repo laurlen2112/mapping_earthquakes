39bb8dce84e5d26233d5c16fdb600fcca4531a78
laurlen2112 <102885345+laurlen2112@users.noreply.github.com>
2022-07-09 14:40:28 -0400

    Add files via upload
    module 13.6.1 to end

---
+++ earthquakes_past7days/js/logicStep1.js
@@ -0,0 +1,55 @@
+console.log("working");
+
+//add tile layer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: API_KEY
+});
+
+
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: API_KEY
+  });
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Satellite: satellite
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [satellite]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+
+
+//create myStyle
+let myStyle = {
+  color: "blue",
+  weight: 1 
+}
+
+// get geo json
+// d3.json(torontoHoods).then(function(data){
+//   console.log(data);
+//   // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJSON(data).addTo(map);
+// });
